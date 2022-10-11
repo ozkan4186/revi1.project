e57ce2dabb89e16054155b62585b55dc00ec16f6
özkan demir <ozkanndemir2009@gmail.com>
2022-10-12 00:01:54 +0300

    Updates

---
+++ precache-manifest.5dab7bc97537b0a17118e7bae08f7078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ed30a0c3c841c8821af53802611233",
+    "url": "/revievs.project/index.html"
+  },
+  {
+    "revision": "cb6f72467e4d038a340d",
+    "url": "/revievs.project/static/css/main.feaa82d2.chunk.css"
+  },
+  {
+    "revision": "849cef09f7fd2d9ef338",
+    "url": "/revievs.project/static/js/2.64741a43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/revievs.project/static/js/2.64741a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6f72467e4d038a340d",
+    "url": "/revievs.project/static/js/main.504d756e.chunk.js"
+  },
+  {
+    "revision": "08df42b1cef2e037244e",
+    "url": "/revievs.project/static/js/runtime-main.2aace4d8.js"
+  }
+]);
